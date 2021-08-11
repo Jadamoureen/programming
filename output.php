3f6403453617e95c255ac802c54865771d07fd1f
David Wampamba <davidwampamba@gmail.com>
2021-08-11 16:15:54 +0300

    How to print in PHP

---
+++ output.php
@@ -0,0 +1,6 @@
+<?php //Option 1. ?>
+<?= "Hello World!\n"; ?>
+<?php //Option 2 ?>
+<?php echo "Hello World!\n"; //If it's a web page use <br/> instead of \n?>
+<?php //Option 2 ?>
+<?php print("Hello World!\n" ); ?>
