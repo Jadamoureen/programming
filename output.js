c9152944de5ba71911bfcdff599fbf8804060a3c
Jadamoureen <32454848+Jadamoureen@users.noreply.github.com>
2021-08-18 22:32:39 +0300

    remove output.js from the repo

--- output.js
+++
@@ -1,7 +0,0 @@
-console.log('The out put will appear at the browser console or CLI console for Node.js')
-
-document.write('The output will appear at the web page where the script is embedded')
-
-document.getElementById('some-id').innerText = 'Output text to a particular HTML element whose id attribute is some-id'
-
-document.querySelector('#some-id-2').innerText = 'Output text to a particular HTML element whose id attribute is some-id-2'
