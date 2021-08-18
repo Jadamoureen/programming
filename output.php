ec9d885035ec8f22588204a5dffd8efaed3ff755
Jadamoureen <32454848+Jadamoureen@users.noreply.github.com>
2021-08-18 22:33:35 +0300

    remove output.php

--- output.php
+++
@@ -1,6 +0,0 @@
-<?php //Option 1. ?>
-<?= "Hello World!\n"; ?>
-<?php //Option 2 ?>
-<?php echo "Hello World!\n"; //If it's a web page use <br/> instead of \n?>
-<?php //Option 2 ?>
-<?php print("Hello World!\n" ); ?>
